bc292c979e6aad5e91889a759f2c616d3f606bd8
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-14 20:19:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(array)
   array.collect{ |obj| puts "#{obj.capitalize}!"
 end
 
-summon_captain_planet(array)
+summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
 def long_planeteer_calls# code an argument here
   # Your code here
