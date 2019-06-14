94059492ee8d95f62f504ccef2fe51e685ca72fb
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-14 20:17:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index{ |name, index| puts "#{index}. #{name}"}
+  array.each_with_index{ |name, index| puts "#{index + 1}. #{name}"}
 end
 
 roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
