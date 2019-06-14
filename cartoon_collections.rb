65772ff5d453eecf236270d31658af2b6fffd2cd
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-14 20:25:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
 def long_planeteer_calls(array)
-  
+  array.any?{ |obj| obj.length > 4}
 end
 
 def find_the_cheese# code an argument here
